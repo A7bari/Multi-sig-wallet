ef5ebc18c411ba6e94adf8b62e6132c3eb326667
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-06 00:54:11 +0100

    add use balance hook

---
+++ src/hooks/useBalance.ts
@@ -0,0 +1,18 @@
+import { formatEther } from "@ethersproject/units";
+import { useWeb3React } from "@web3-react/core";
+import { useEffect, useState } from "react";
+
+export const useBalance = () => {
+  const { account, library } = useWeb3React();
+  const [ balance , setBalance ] = useState();
+
+  useEffect(()=> {
+    //@ts-ignore
+    library?.eth.getBalance(account).then((result)=>{
+      setBalance(result)
+    })
+
+  }, [ account, library ])
+
+  return  balance ? formatEther(balance) : null ;
+}
