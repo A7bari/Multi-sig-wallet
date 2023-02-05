ac84ea1ca008afe79c2b4014060a19476bf4d482
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-06 00:53:02 +0100

    add web3Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
+import { Web3ReactProvider } from '@web3-react/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import Web3 from 'web3';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const getLibrary = (provider: any) => {
+  return new Web3(provider);
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Web3ReactProvider getLibrary={getLibrary} >  
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Web3ReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
