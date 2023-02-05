31eefb30cb96dd9d110e9abb4a8a67d324e84b10
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-06 00:53:59 +0100

    add details components

---
+++ src/components/AccountDetails.tsx
@@ -0,0 +1,50 @@
+interface Props{
+  chainId: number | undefined,
+  account: string ,
+  balance: string
+}
+
+const copyTextToClipboard = async(text: string) => {
+  if ('clipboard' in navigator) {
+    return await navigator.clipboard.writeText(text);
+  } else {
+    return document.execCommand('copy', true, text);
+  }
+}
+
+
+const AccountDetails = ({chainId, account, balance}: Props) => {
+
+  const handleCopyClick = () => {
+    copyTextToClipboard(account as string)
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className='details'>
+       <div className={chainId === 1 ? 'network' : 'network warning'}>
+        {chainId === 1 ? "connected to Ethereum's Mainnet" : "Please connect to Ethereum's Mainnet"}
+      </div>
+
+      <div className='dataSpace account' onClick={handleCopyClick} >
+        <h3 className='title'>account</h3> 
+        <div className='account-number'>
+          { account.slice(0,6) + "..." + account.slice(-6)}
+          <span className="copy-icon">
+            <img src='./icons/copy.svg' alt="copy account adress" />
+          </span>
+        </div> 
+      </div>
+     
+      <div className='dataSpace balance'>
+        <h5 className='title'>Total balance</h5> 
+        { balance } eth
+        <img src="./icons/ETH-icon2.png" alt="" className="eth-icon"/>
+      </div>
+    </div>
+  )
+}
+
+export default AccountDetails
