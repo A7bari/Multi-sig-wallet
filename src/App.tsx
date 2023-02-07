66f247b44ac7a1e9334568995dfbb08bda0c9964
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-08 00:34:27 +0100

    update the app comp.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
+import { useEffect} from 'react';
 import './App.css';
 import { useWeb3React } from '@web3-react/core';
-import ConnectWallet from './components/ConnectWallet';
-import { useBalance } from './hooks/useBalance';
-import AccountDetails from './components/AccountDetails';
+import { injected } from './connector/connector';
+import { Stack } from '@mui/system';
+import { LinearProgress, Typography } from '@mui/material';
+import MainLayout from './components/MainLayout/MainLayout';
 
 function App() {
 
-  const { account , chainId, active } = useWeb3React();
-  const balance = useBalance();
-  console.log({account})
+  const { activate, active } = useWeb3React();
+
+  const connect = async() => {
+    try {
+      await activate(injected) ;
+    } catch (error) {
+      console.log({error})
+    }
+  }
+
+  useEffect(() => {
+    !active && connect();
+  }, [active])
+  
+
   return (
-    <div className="container">
-      <div className='app'>
-        {active && (
-          <AccountDetails chainId={chainId} account={account as string} balance={balance || "0.0"} />
-          )}
-          <ConnectWallet />
-      </div>
-    </div>
+    !active 
+      ? <Stack height={'100vh'} justifyContent={'center'} alignItems={'center'} spacing={2}>
+          <Typography variant='h2'>Connecting Your Wallet</Typography>
+          <Typography variant='body1' color={'primary.light'}>Please wait while we securely connect to your wallet.</Typography>
+          <Stack width={'100%'}><LinearProgress /></Stack>
+          <Typography variant='body2'>Make sure you are using Metamask: The wallet you are connecting must be Unlocked</Typography>
+        </Stack>
+      : <MainLayout/>
+    
   );
 }
 
