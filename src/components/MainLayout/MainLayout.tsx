a0e740aad2098cf067e33d195c5276239abcaacb
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-08 16:03:58 +0100

    update main layout

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Box, Toolbar } from '@mui/material';
+import { Box, Toolbar, useMediaQuery, Theme } from '@mui/material';
 import Header from '../Header';
+import { Stack } from '@mui/material';
+import NavMenu from './../Navigation/NavMenu';
+import { Container } from '@mui/system';
 
 function MainLayout() {
+   const matchesXs = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   return (
    <>
       <Header />
       <Box component="main" sx={{ width: '100%' }}>
          <Toolbar />
+         <Container>
+            <Stack  direction={'row'}>
+               {!matchesXs && <Box sx={{maxWidth: 300}}>
+                              <NavMenu />
+                           </Box>}
+
+            </Stack>
+         </Container>
       </Box>
    </>
   )
