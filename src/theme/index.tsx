71977c1ef659fd2e550a5f0651573958b7dfce2e
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-08 16:03:31 +0100

    update theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,57 +1,65 @@
 
-import {PropsWithChildren, useMemo } from 'react';
+import {PropsWithChildren } from 'react';
 
 // material-ui
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { CssBaseline, StyledEngineProvider, Theme } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { grey } from '@mui/material/colors';
 
-const Palette = () => {
+function Paper() {
+  return {
+      MuiPaper: {
+          styleOverrides: {
+              root: {
+                backgroundImage: 'none'
+              }
+          }
+      }
+  };
+}
 
-
-   return createTheme({
-       palette: {
-           mode : 'dark',
-           common: {
-             black: '#090B1C',
-             white: '#F6F6F6',
-           },
-           primary: {
-             light: '#dbfba8',
-             main: '#C7FF6C',
-           },
-           secondary: {
-             main: '#ffdd6c',
-           },
-           text: {
-             primary: '#C7FF6C',
-             secondary: '#aeaeae',
-           },
-           action: {
-             disabled: grey[300]
-           },
-           divider: '#141B43',
-           background: {
-             default: '#0E0F10',
-             paper: '#030305', 
-           }
-       }
-   });
-};
 
 // ==============================|| DEFAULT THEME - MAIN  ||============================== //
 
 export default function ThemeCustomization({ children }: PropsWithChildren) {
-    const theme = Palette();
 
-    const themeOptions = useMemo(   
-        () => ({
-            palette: theme.palette,
-        }),
-        [theme]
-    );
+    const themes = createTheme({
+        palette: {
+          mode : 'dark',
+          common: {
+            black: '#090B1C',
+            white: '#F6F6F6',
+          },
+          background: {
+            default: '#121827',
+            paper: '#1b2130'
+          },
+          divider: '#E6E8F0',
+          primary: {
+            main: '#0db981',
+            light: '#18b79e'
+          },
+          secondary: {
+            main: '#fa8c01',
+            light: '#ffb01f'
+          },
+          text: {
+            primary: '#fdfeff',
+            disabled: '#9ba3a0'
+          },
+          action: {
+            disabled: 'rgba(55, 65, 81, 0.26)',
+            selectedOpacity: 0.08
+          }
+      },
+      typography: {
+        fontFamily: `'Inter', 'Segoe UI', 'sans-serif'`,
+        button: {
+          fontWeight: 600
+        },
+      }
+    });
+    themes.components = Paper()
 
-    const themes = createTheme(themeOptions);
     return (
        <StyledEngineProvider injectFirst>
             <ThemeProvider theme={themes}>
