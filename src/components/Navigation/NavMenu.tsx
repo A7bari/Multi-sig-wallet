ffd206cffa2c534047c9ec8ca1679691ba66446c
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-09 18:07:45 +0100

    update navigation menu

--- src/components/Navigation/NavMenu.tsx
+++ src/components/Navigation/NavMenu.tsx
@@ -3,18 +3,23 @@ import { List, Paper, ListItem, ListItemButton, ListItemIcon, ListItemText, SvgI
 import { navItems } from '../../helpers/const';
 import { Divider } from '@mui/material';
 import { useTheme } from '@mui/material';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function NavMenu() {
-   const selected = 1;
+   const location = useLocation();
    const theme = useTheme();
+   const navigate = useNavigate();
+  
   return (
-   <Paper sx={{p:2, mt: 3, backgroundImage: 'none',}}> 
+   <Paper sx={{p:2, mt: 3, backgroundImage: 'none',minWidth: 290}}> 
       <List>
-         {navItems.map((item, i) => (
-            <ListItem key={i} >
+         {navItems.map((item, i) => {
+            const isSelected = item.url === location.pathname;
+
+            return <ListItem key={i} >
                   <ListItemButton 
-                     onClick={() => {}}
-                     selected={selected === i}
+                     onClick={() => navigate(item.url)}
+                     selected={isSelected}
                      sx={{
                         px:2, 
                         borderRadius: 1, 
@@ -34,11 +39,11 @@ function NavMenu() {
                      )}
                      <ListItemText 
                         primary={item.label} 
-                        primaryTypographyProps={{fontWeight: (selected === i)? 600: 500, color: (selected === i) ? 'primary.main': ''}}
+                        primaryTypographyProps={{fontWeight: isSelected? 600: 500, color: isSelected ? 'primary.main': ''}}
                      />
                   </ListItemButton>
             </ListItem>
-         ))} 
+         })} 
       </List>
    </Paper>
   )
