3432ba6b5198d61f7addca3ff390ebb2db3eca13
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-09 18:08:40 +0100

    add tramsactions list page

---
+++ src/pages/Transactions/Transactions.tsx
@@ -0,0 +1,148 @@
+import { Button, Card, Chip, Container, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import useMultisigContract from 'hooks/useMultisigContract';
+import  {useState, useEffect} from 'react'
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useWeb3React } from '@web3-react/core';
+
+interface Transaction{
+   to : string,
+   value: number,
+   data: string,
+   executed: boolean,
+   nbrApprovals:number
+}
+
+const headLabel = [
+   'number', 'to address', 'value', 'data', 'excuted', 'confirmations' , ''
+]
+
+function Transactions() {
+   const {contract} = useMultisigContract()
+   const [transactions , setTransactions ] = useState<Transaction[]>([])
+   const [nbrApprovalsRequired, setNbrApprovalsRequired] = useState(0);
+   const [performing , setPerforming] = useState(true);
+   const [errMsg , setErrMsg] = useState<string|null>(null);
+   const {account} = useWeb3React();
+
+
+   useEffect( () => {
+      const getowners = async () => {
+         try {
+            console.log(contract)
+            const res = await contract?.methods.getTransactions().call({from: account}) as Transaction[];
+            const nbr = await contract?.methods.NbrApprovalRequired().call({from: account})
+            setTransactions(res);
+            setNbrApprovalsRequired(nbr);
+         } catch (error) {
+            setErrMsg('something went wrong');
+         } finally {
+            setPerforming(false);
+         }
+      }
+      getowners()
+
+      return () => {
+         setTransactions([])
+         setPerforming(true)
+      }
+   }, [contract])
+
+   function handleApprove() {
+
+   } 
+
+  return (
+   <Container sx={{mt: 6}}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h5" gutterBottom>
+            All transactions
+          </Typography>
+          <Button variant="contained" size='small'>
+            New transaction
+          </Button>
+        </Stack>
+
+        <Card>
+        {(performing)
+         &&  (
+                     <Paper
+                        sx={{
+                           textAlign: 'center',
+                        }}
+                     >
+                        <CircularProgress />
+                     </Paper>
+
+         )}
+         { !!transactions 
+            && (
+            <TableContainer sx={{ minWidth: 800 }}>
+               <Table sx={{ border: '1px solid',
+                     borderColor: 'secondary.main'}}>
+               <TableHead 
+                  sx={{
+                     backgroundColor: 'background.default',
+                  }}
+               >
+                  <TableRow>
+                     {headLabel.map((headCell,i) => (
+                        <TableCell
+                           key={i}
+                           align={'center'}
+                        >
+                           {headCell}
+                        </TableCell>
+                     ))}
+                  </TableRow>
+               </TableHead>
+               <TableBody>
+                  {transactions.map((tx, index) => {
+                     const { to, value, data, executed, nbrApprovals} = tx;
+                     const canBeApproved = nbrApprovalsRequired <= nbrApprovals ;
+                     return (
+                     <TableRow hover key={index} tabIndex={-1}  selected={false}>
+
+                        <TableCell component="th" scope="row" padding="none" align='center'>
+                           <Typography variant="subtitle2" noWrap color={'primary.main'} sx={{fontWeight: 700}}>
+                              {index}
+                           </Typography>
+                        </TableCell>
+
+                        <TableCell align="left">{to}</TableCell>
+
+                        <TableCell align="center">{value}</TableCell>
+
+                        <TableCell align="center">{data}</TableCell>
+
+                        <TableCell align="center">{executed ? 'Yes' : 'No'}</TableCell>
+
+                        <TableCell align="center">
+                           <Chip  variant="outlined" label={nbrApprovals} color={(!canBeApproved && 'warning') || 'success'} />
+                        </TableCell>
+
+                        <TableCell align="right">
+                           <Button 
+                              size="small" 
+                              onClick={handleApprove} 
+                              variant='outlined' 
+                              startIcon={<AddTaskIcon />}
+                           >
+                              Approve
+                           </Button>
+                        </TableCell>
+                     </TableRow>
+                     );
+                  })}
+               </TableBody>
+               </Table>
+            </TableContainer>
+         )}
+
+        </Card>
+      </Container>
+  )
+}
+
+export default Transactions
