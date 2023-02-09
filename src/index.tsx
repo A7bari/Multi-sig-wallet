26bf0b999cf576746f604a535370581eab2dba42
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-09 18:10:28 +0100

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { Web3ReactProvider } from '@web3-react/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Web3 from 'web3';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,13 +15,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ThemeCustomization>
-    <Web3ReactProvider getLibrary={getLibrary} >  
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Web3ReactProvider>
-  </ThemeCustomization>
+  <React.StrictMode>
+    <ThemeCustomization>
+      <BrowserRouter>
+        <Web3ReactProvider getLibrary={getLibrary} >  
+            <App />
+        </Web3ReactProvider>
+      </BrowserRouter>
+    </ThemeCustomization>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
