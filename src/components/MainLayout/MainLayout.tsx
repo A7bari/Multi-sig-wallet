8bc59b39518a907e1243b751677df4e5fefe6b1a
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-02-09 18:09:57 +0100

    add routes

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -4,6 +4,11 @@ import Header from '../Header';
 import { Stack } from '@mui/material';
 import NavMenu from './../Navigation/NavMenu';
 import { Container } from '@mui/system';
+import {Routes, Route } from 'react-router-dom';
+import Owners from 'pages/Owners';
+import Wallet from 'pages/Wallet';
+import SubmitTransaction from 'pages/SubmitTransaction';
+import Transactions from 'pages/Transactions';
 
 function MainLayout() {
    const matchesXs = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
@@ -17,6 +22,12 @@ function MainLayout() {
                {!matchesXs && <Box sx={{maxWidth: 300}}>
                               <NavMenu />
                            </Box>}
+               <Routes>
+                  <Route path={'/owners'} element={<Owners />} />
+                  <Route path={'/transactions'} element={<Transactions />} />
+                  <Route path={'/wallet'} element={<Wallet />} />
+                  <Route path={'/submitTansaction'} element={<SubmitTransaction />} />
+               </Routes>
 
             </Stack>
          </Container>
